1835f5ab5a11e19fb9ed7e0361704b9e76aaa93b
--Hugo Quinteiro <hugoquinteiro.mq@gmail.com>
2021-12-09 10:45:35 -0300

    grava pela base

--- base1.js
+++ base1.js
@@ -1,10 +1,94 @@
 var login = require('./login')
+const axios = require('axios')
+
+
+const order = {
+   "serviceName":"CACSP.incluirNota",
+   "requestBody":{
+      "nota":{
+         "cabecalho":{
+            "NUNOTA":{
+            },
+            "CODPARC":{
+               "$":"9999"
+            },
+            "DTNEG":{
+               "$":"09/12/2021"
+            },
+            "CODTIPOPER":{
+               "$":"3100"
+            },
+            "CODTIPVENDA":{
+               "$":"1"
+            },
+            "CODVEND":{
+               "$":"7"
+            },
+            "CODEMP":{
+               "$":"12"
+            },
+            "CODNAT":{
+               "$":"1010101"
+            },            
+            "TIPMOV":{
+               "$":"P"
+            }
+         },
+         "itens":{
+            "INFORMARPRECO":"True",
+            "item":[
+                  {
+                   "NUNOTA":{
+                  },
+                  "CODPROD":{
+                     "$":"147"
+                  },
+                  "QTDNEG":{
+                     "$":"1"
+                  },
+                  "CODLOCALORIG":{
+                     "$":"2"
+                  },
+                  "VLRUNIT":{
+                     "$":"201"
+                  },
+                  "PERCDESC":{
+                     "$":"0"
+                  },                  
+                  "CODVOL":{
+                     "$":"UN"
+                  }
+               }               
+            ]
+         }
+      }
+   }
+}
 
 
 login.then((res) => {
 	var session = res
-	return session
+	console.log ('session', session)
+    const urlPed = axios.create({
+      baseURL: `http://192.168.1.56:8280/mgecom/service.sbr?serviceName=CACSP.incluirNota&mgeSession=${session}&outputType=json`
+    });
+
+async function incluirPed (req) {
+
+  try{
+    const {data} = await urlPed.post(null, order);
+    var dados = data;
+    console.log('data', data)
+  } catch(error) {
+    console.log(error)
+  }    
+}    
+//console.log('urlPed:',urlPed)
+console.log('Incluir Pedido')
+incluirPed();
+
+
 })
-.then((res2) => console.log('Retorno Login:', res2))
+
 
 
