85ab563827596531dc2ccd01a4cf8222436f4206
Hugo Quinteiro <hugoquinteiro.mq@gmail.com>
2022-01-04 17:22:43 -0300

    preparando itens para gravar no cache

--- index.js
+++ index.js
@@ -1,56 +1,52 @@
 const express = require("express");
+const session = require('express-session')
+const bodyParser = require('body-parser')
 const app = express();
-const port = 8001
 const select = require('./pgsql/select')
-//const insert = require('./db/insert')
-//const sqlins = require('./db/insertML')
+const port = 8001
+
 
 // Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
+  //Implementação do session
+  app.use(session({secret:'abc@123'}))
+  //Carregando bodyParser
+  app.use(bodyParser.urlencoded({extended:true}))
+  app.use(bodyParser.json())
 
 // Rotas
 app.get("/",(req, res) => {
   res.render('index')
 });
 
-var temp = {}
 
-temp.force = [
-              {prod:123, vlrvenda: 10,descrprod: 'Prod Force'},
-              {prod:321, vlrvenda: 200,descrprod: 'Force 2'},
-],
-temp.mqPro = [{prod:456,vlrvenda: 599,descrprod: 'Prod MQ PRO'}]              
 
 //Pedido
 app.get("/pedido",(req, res) => {
 
   select('produto', 'marca').then(produtos => {
     var v_marca 
-    var marcas = []
-    var tempMarcas = {}
-    var i = 0
     produtos.forEach(function (valor, indice){
-      if(v_marca!=produtos[indice].marca){
-        //marcas.push(produtos[indice].marca)
-        tempMarcas = produtos[indice].marca
-        i++
-        marcas.push({tempMarcas})
-      }
       v_marca = produtos[indice].marca
-      
     })
-    //console.log(produtos)
-    
-    //console.log(marcas)
 
-
-    //console.log({produto : produtos, marca: marcas})
     res.render('pedido', { produto : produtos})
   })
 
 });
 
+session.vendas = []
+
+//Gravar Item
+app.post('/gravarItem', (req, res) => {
+  session.vendas.push(req.body)
+})
+
+//Salvar Pedido
+app.post('/salvarPedido', (req, res) => {
+  console.log(session.vendas)
+})
 
 app.listen(port,()=>{console.log("App rodando em ", port);})
--- public/js/pedido.js
+++ public/js/pedido.js
@@ -1,16 +1,46 @@
 console.log('Pedido Carregou')
-
-
+var myModal = new bootstrap.Modal(document.getElementById('addItemModal'), {})
 
 function loadFormItem(codprod){
-  var myModal = new bootstrap.Modal(document.getElementById('addItemModal'), {})
+  
   var item = document.getElementById('item-'+codprod).innerHTML
   var preco = document.getElementById('preco-'+codprod).innerHTML
   var estoque = document.getElementById('estoque-'+codprod).innerHTML
   var estInfo = document.getElementById('estInfo')
+  var precoItem = document.getElementById('precoItem')
+  
   var tittle = document.getElementById('addItemModalTittle')
   tittle.innerText = item
   estInfo.innerHTML = estoque
+  precoItem.value = preco
   myModal.toggle()
+}
 
+
+function gravarItem(){
+  console.log('Gravar Item')
+  var item = document.getElementById('addItemModalTittle').textContent;
+  var quantidade = document.getElementById('quantidade').value;
+  var precoItem = document.getElementById('precoItem').value;
+
+  var dadosItem = {
+    produto : item,
+    quantidade : quantidade,
+    preco:  precoItem
+  }
+
+  myModal.hide()
+
+  axios.post('gravarItem', {dados: dadosItem})
+  .then(resp => {
+    console.log('Fechar Modal')
+  })
+}
+
+function salvarPedido(){
+  console.log('Salvar Pedido')
+  axios.post('salvarPedido', {salvar: 'ok'})
+  .then(resp => {
+    console.log('then para fechar pedido')
+  })
 }
