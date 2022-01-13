f323f1ed4d95c33d989a2cad0c40294e2d79c96f
--Hugo Quinteiro <hugoquinteiro.mq@gmail.com>
2022-01-13 18:28:10 -0300

    tela de pedido com table

--- public/js/pedido.js
+++ public/js/pedido.js
@@ -23,17 +23,26 @@ function gravarItem(){
   var quantidade = document.getElementById('quantidade').value;
   var precoItem = document.getElementById('precoItem').value;
 
+  
+  //console.log(item.substring(0, item.indexOf(' -')).trim(), item.substring(item.indexOf('- ')+2))
+  //Separando codigo da descrição
+  var codprod = item.substring(0, item.indexOf(' -')).trim()
+  var descrprod = item.substring(item.indexOf('- ')+2)
+
   var dadosItem = {
-    produto : item,
+    codprod : codprod,
+    descrprod : descrprod,
     quantidade : quantidade,
     preco:  precoItem
   }
 
+//  atualizaTableItens(dadosItem)
   myModal.hide()
 
   axios.post('gravarItem', {dados: dadosItem})
   .then(resp => {
-    console.log('Gravar Item', resp)
+    //console.log('Gravar Item', resp.data)
+    atualizaTableItens(resp.data)
   })
   .catch( err => {
     console.log('erro item: ', err)
@@ -44,9 +53,37 @@ function salvarPedido(){
   console.log('Salvar Pedido')
   axios.post('salvarPedido', {salvar: 'ok'})
   .then(resp => {
-    console.log('Gravar pedido', resp)
+    //console.log('Gravar pedido', resp)
   })
   .catch( err => {
     console.log('erro pedido: ', err)
   })
+}
+
+
+function atualizaTableItens (arrItens) {
+  tbodyItens.innerHTML=''
+  //Carregando linha de item na tabela
+  arrItens.forEach(el => {
+    console.log(el)
+    let tbody = document.getElementById('tbodyItens')
+    let tr = tbody.insertRow()
+  
+    let item = Object.values(el)
+  
+    let td_codprod = tr.insertCell()
+    let td_descrprod = tr.insertCell()
+    let td_vlrUnit = tr.insertCell()
+    let td_qtd = tr.insertCell()
+    let td_vlrTotal = tr.insertCell()
+  
+    td_codprod.innerText = item[0]
+    td_descrprod.innerText = item[1]
+    td_vlrUnit.innerText = item[3].toLocaleString('pt-br', {minimumFractionDigits: 2, maximumFractionDigits:2})
+    td_qtd.innerText = item[2]
+    td_vlrTotal.innerText = (item[3] * item[2]).toLocaleString('pt-br', {minimumFractionDigits: 2, maximumFractionDigits:2})
+  
+  });
+
+
 }
